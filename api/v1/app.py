8e157743fc66a65648b96d42db0761978ac16803
Tonny Batya <84370752+devbxtzz@users.noreply.github.com>
2022-11-10 12:38:20 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0:5002"}})
 
 
 @app.teardown_appcontext
@@ -37,6 +37,6 @@ Swagger(app)
 
 
 if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
+    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0:5002"
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5002
     app.run(host=host, port=port, threaded=True)
